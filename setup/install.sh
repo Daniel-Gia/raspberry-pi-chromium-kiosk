fd2603901be4ca0369542bf3a8245513d7ae59e4
Daniel <114685295+Daniel-Gia@users.noreply.github.com>
2026-01-09 20:18:07 +0100

    Save env into .env file and use it everywhere
    this fixes the problems with env introduced in #29 and #26

--- setup/install.sh
+++ setup/install.sh
@@ -63,10 +63,6 @@ else
     echo "Pinned Docker image to: latest"
 fi
 
-# Make IMAGE_TAG permanent so that we dont pull latest unintentionally in future runs
-echo "IMAGE_TAG=$RELEASE_TAG" | sudo tee -a /etc/environment
-source /etc/environment
-
 echo "Running setup script..."
 cd "$INSTALL_DIR"
 chmod +x setup/setup.sh
